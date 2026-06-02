75db688f00f7b1f9e1db800e1535f4a12e69f06f
uranikhodajj <uranik.hodaj@student.uni-pr.edu>
2026-06-02 18:03:10 +0200

    Update simulator.py

--- sensor-service/simulator.py
+++ sensor-service/simulator.py
@@ -4,7 +4,6 @@ import random
 from datetime import datetime
 from kafka import KafkaProducer
 
-# Konfigurimi i Kafka Producer
 producer = KafkaProducer(
     bootstrap_servers=['localhost:9092'],
     value_serializer=lambda v: json.dumps(v).encode('utf-8')
