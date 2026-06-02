41a92497c1acbffb5cae47eb918cc4291ce84451
VeseCikaqi <vese.cikaqi@student.uni-pr.edu>
2026-06-02 18:09:41 +0200

    Update frontend-dashboard

--- frontend-dashboard/app.py
+++ frontend-dashboard/app.py
@@ -35,16 +35,15 @@ while True:
             filtered_df = df[df['Qyteti'].isin(qytetet_e_zgjedhura)]
             filtered_df = filtered_df.sort_values(by="timestamp")
             
-            # --- Vizualizimet ---
             col1, col2 = st.columns(2)
             
             with col1:
-                st.subheader("🌡️ Trendi i Temperaturave")
+                st.subheader("Trendi i Temperaturave")
                 temp_chart_data = filtered_df.pivot(index='timestamp', columns='Qyteti', values='temperature')
                 st.line_chart(temp_chart_data)
                 
             with col2:
-                st.subheader("💧 Trendi i Lagështisë")
+                st.subheader("Trendi i Lagështisë")
                 hum_chart_data = filtered_df.pivot(index='timestamp', columns='Qyteti', values='humidity')
                 st.line_chart(hum_chart_data)
                 
@@ -53,8 +52,8 @@ while True:
             st.dataframe(filtered_df.tail(10).reset_index(drop=True), use_container_width=True)
             
         elif len(qytetet_e_zgjedhura) == 0:
-            st.warning("⚠️ Ju lutem zgjidhni të paktën një qytet nga menyja anësore.")
+            st.warning("Ju lutem zgjidhni të paktën një qytet nga menyja anësore.")
         else:
-            st.info("⏳ Duke pritur për të dhëna nga sistemi Kafka/Spark...")
+            st.info("Duke pritur për të dhëna nga sistemi Kafka/Spark...")
             
     time.sleep(2)
